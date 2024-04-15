8241ee9e6af8f0ac7808e7b5e5b86a98eceb3045
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-04-15 14:06:02 +0200

    job05 done

---
+++ jour01/job05/script.js
@@ -0,0 +1,7 @@
+function afficherJoursSemaines() {
+    let jours = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+    for (let i = 0; i < jours.length; i++) {
+        console.log(jours[i]);
+    }
+}
+afficherJoursSemaines();
